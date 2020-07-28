13195a4a49ed33e9f85530c8220cef407fb52b84
Thomas Aldaitz <taldaitz@dawan.fr>
2020-07-28 08:38:42 +0200

    Ajout des supports

--- formation/src/Controller/HomeController.php
+++ formation/src/Controller/HomeController.php
@@ -19,6 +19,11 @@ class HomeController extends AbstractController
         ]);
     }
 
+    public function notFound()
+    {
+        return new Response("Désolé, cette page n'existe pas !");
+    }
+
     
     /**
      * @Route("/hello/{chiffre}", name="salutation", requirements={"chiffre"="\d+"})
@@ -35,5 +40,34 @@ class HomeController extends AbstractController
         return new Response('Bonjour ' . $prenom);
     }
 
+    /**
+     * @Route("/addition/{chiffre1}/{chiffre2}", name="addition",
+     * requirements={"chiffre1"="\d+", "chiffre2"="\d+"})
+     */
+    public function addition($chiffre1, $chiffre2) {
+        $resultat = $chiffre1 + $chiffre2;
+        return new Response("Le résultat est $resultat");
+    }
+
+
+    /**
+     * @Route("/contact/nom/{nom}/prenom/{prenom}", name="contact")
+     */
+    public function contact($nom, $prenom) {
+        return new Response("Nom : $nom <br>
+                            Prenom: $prenom");
+    }
+
+
+    /**
+     * @Route("/addition/{chiffre1}+{chiffre2}", name="additionPlus")
+     */
+    public function additionPlus($chiffre1, $chiffre2) 
+    {
+        //return $this->redirect("/addition/$chiffre1/$chiffre2");
+
+        return $this->redirectToRoute('addition', ['chiffre1' => $chiffre1, 
+                                        'chiffre2' => $chiffre2]);
+    }
     
 }
